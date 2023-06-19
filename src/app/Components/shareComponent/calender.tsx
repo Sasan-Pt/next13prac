63123d7324fd7b53526baa248ee843c401500b9e
Sasan Poor <sasan.pt.tec@gmail.com>
2023-06-19 22:48:19 +0330

    adding calender

--- src/app/Components/shareComponent/calender.tsx
+++ src/app/Components/shareComponent/calender.tsx
@@ -1,26 +1,64 @@
 "use client"
+import { data } from "autoprefixer";
 import {useEffect,useState} from "react"
 const Calender=()=>{
     const [currentTime,setCurrentTime]=useState<string>()
     useEffect(() => {
         setCurrentTime(() => {
-          const timeServer = ("0" + new Date);
-    
+          const timeServer =  ("0" + new Date)
+
           return `${timeServer}`;
         });
       }, []);
 
+      console.log(currentTime)
 
-      const serverTime = currentTime
-       const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+      const serverTime = new Date();
+      const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
       const options = { timeZone: userTimeZone };
       const userLocalTime = serverTime?.toLocaleString(undefined, options);
+      const test=serverTime?.getDate()
+      console.log(test)
+      console.log(userLocalTime?.slice(17))
+
+
+      
+      function getDaysInMonth(year, month) {
+        const numDays = new Date(year, month, 0).getDate();
+        const days = [];
+      
+        for (let i = 1; i <= numDays; i++) {
+          days.push(i);
+        }
+      
+        return days;
+      }
+      
+      // Example usage
+      const year = 2023;
+      const month = 7; // Note: January is month 0, February is month 1, and so on
+      
+      const daysInMonth = getDaysInMonth(year, month);
+      console.log(daysInMonth); 
+
+      function getMonthName(monthh) {
+        const date = new Date(2000, month, 1);
+        const options = { month: 'long' };
+        return date.toLocaleString('en-US', options);
+      }
+      
+      // Example usage
+      const monthh = 2; // Note: January is month 0, February is month 1, and so on
+      
+      const monthName = getMonthName(monthh);
+      console.log(monthName); 
+
 
     return(
         <div className="bg-[#202020]">
             <span>
-                <h1>Estemated Time</h1>
-                { <div className="text-white">{userLocalTime}</div> }
+                <h1 className="text-white">Estemated Time</h1>
+                <div className="text-white">{userLocalTime?.slice(17)}</div>
             </span>
         </div>
     )
